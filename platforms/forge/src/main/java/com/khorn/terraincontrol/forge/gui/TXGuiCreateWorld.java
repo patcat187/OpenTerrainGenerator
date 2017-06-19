12c758fcfa2f6a02aa0fce25883fee4adfef2989
PG85 <rik_lubking@hotmail.com>
2017-06-20 00:35:44 +0200

    v21: Abbreviations for BO3's, world creation screen auto-select
    - Abbreviations for resources in BO3's are now allowed, for instance B()
    instead of Block(), BR() instead of Branch(), etc.
    - When the world creation menu is shown for the first time the first
    preset (if any) is automatically selected.

--- platforms/forge/src/main/java/com/khorn/terraincontrol/forge/gui/TXGuiCreateWorld.java
+++ platforms/forge/src/main/java/com/khorn/terraincontrol/forge/gui/TXGuiCreateWorld.java
@@ -876,7 +876,6 @@ public class TXGuiCreateWorld extends GuiScreen implements GuiYesNoCallback
         
         // World name
         this.txtWorldName = new GuiTextField(30, this.fontRendererObj, this.width / 2 - 164, 45, 160, 20); //left, top, width, height           
-        this.txtWorldName.setText(GuiHandler.selectedWorldName != null ? GuiHandler.selectedWorldName : GuiHandler.worldName != null ? GuiHandler.worldName : I18n.format("selectWorld.newWorld", new Object[0]));
         this.txtWorldName.setEnabled(false);
         
         // Seed
@@ -935,6 +934,22 @@ public class TXGuiCreateWorld extends GuiScreen implements GuiYesNoCallback
         btnCreateWorld = new GuiButton(0, this.width / 2 - 166, 213, 164, 20, I18n.format("selectWorld.create", new Object[0]));
         this.buttonList.add(btnCreateWorld);
         this.buttonList.add(new GuiButton(1, this.width / 2 + 2, 213, 164, 20, I18n.format("gui.cancel", new Object[0])));
+        
+    	if(GuiHandler.selectedWorldName != null)
+		{
+    		this.txtWorldName.setText(GuiHandler.selectedWorldName); 
+		}
+		else if (GuiHandler.worldName != null)
+		{
+			this.txtWorldName.setText(GuiHandler.worldName);
+		} else {
+    		if(btnavailableWorld1.displayString.length() > 0 && !btnavailableWorld1.displayString.equalsIgnoreCase(""))
+    		{
+    			this.txtWorldName.setText(btnavailableWorld1.displayString);
+			} else {
+				this.txtWorldName.setText(I18n.format("selectWorld.newWorld", new Object[0]));
+			}
+		}
         
         this.updateWorldName();
         this.updateButtons();

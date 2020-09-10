d0a0006e61bcaa8fe3ac8ccd5b61cdf78a9cf4e0
PG85 <rik_lubking@hotmail.com>
2020-09-11 00:30:01 +0200

    1.16.x 1.0.0_r1: Basic console command

--- platforms/forge/src/main/java/com/pg85/otg/forge/OTGPlugin.java
+++ platforms/forge/src/main/java/com/pg85/otg/forge/OTGPlugin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.DimensionSettings;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -24,6 +25,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.pg85.otg.forge.biome.OTGBiomeProvider;
+import com.pg85.otg.forge.commands.OTGCommand;
 import com.pg85.otg.forge.generator.OTGNoiseChunkGenerator;
 
 import java.util.stream.Collectors;
@@ -34,7 +36,7 @@ public class OTGPlugin
 {
 	// TODO: Use Forge API to register DimensionSettings?
 	public static final RegistryKey<DimensionSettings> field_242734_c = RegistryKey.func_240903_a_(Registry.field_243549_ar, new ResourceLocation("overworld"));
-	
+
 	// Register the otg worldtype for the world creation screen
 	public static final BiomeGeneratorTypeScreens otgWorldType = new BiomeGeneratorTypeScreens("otg")
 	{
@@ -49,12 +51,12 @@ public class OTGPlugin
 			);
 		}
 	};
-	
+
 	// Directly reference a log4j logger.
 	private static final Logger LOGGER = LogManager.getLogger();
-	
+
 	public OTGPlugin()
-	{
+	{		
 		// Register the setup method for modloading
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		// Register the enqueueIMC method for modloading
@@ -68,34 +70,41 @@ public class OTGPlugin
 		MinecraftForge.EVENT_BUS.register(this);
 		
 		// Register the otg worldtype for the world creation screen
-		BiomeGeneratorTypeScreens.field_239068_c_.add(otgWorldType);
+		BiomeGeneratorTypeScreens.field_239068_c_.add(otgWorldType);	
 	}
-	
+
 	private void setup(final FMLCommonSetupEvent event)
 	{
 		// some preinit code
 		LOGGER.info("HELLO FROM PREINIT");
 		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
 	}
-	
+
 	private void doClientStuff(final FMLClientSetupEvent event)
 	{
 		// do something that can only be done on the client
 		LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
 	}
-	
+
 	private void enqueueIMC(final InterModEnqueueEvent event)
 	{
 		// some example code to dispatch IMC to another mod
 		InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
 	}
-	
+
 	private void processIMC(final InterModProcessEvent event)
 	{
 		// some example code to receive and process InterModComms from other mods
 		LOGGER.info("Got IMC {}", event.getIMCStream().
 		map(m->m.getMessageSupplier().get()).
 		collect(Collectors.toList()));
+	}
+
+	
+	@SubscribeEvent
+	public void onCommandRegister(RegisterCommandsEvent event)
+	{
+		OTGCommand.register(event.getDispatcher());
 	}
 	
 	// You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -105,7 +114,7 @@ public class OTGPlugin
 		// do something when the server starts
 		LOGGER.info("HELLO from server starting");
 	}
-	
+
 	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
 	// Event bus for receiving Registry Events)
 	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
